f34a4f6f0b511bf282264b90cd6764aa22f06319
nepsoul <nirumagar901@gmail.com>
2023-02-06 20:19:07 +0545

    chore: initial setup for form to use custom hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import useCounter from "./useCounter";
+import { useState } from "react";
 
-const App = (props) => {
-  const counter = useCounter();
-  const counterBottom = useCounter();
+const App = () => {
+  const [name, setName] = useState("");
+  const [born, setBorn] = useState("");
+  const [height, setHeight] = useState("");
   return (
-    <>
+    <div>
+      <form>
+        name:
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br />
+        birthdate:
+        <input
+          type="date"
+          value={born}
+          onChange={(e) => setBorn(e.target.value)}
+        />
+        <br />
+        height:
+        <input
+          type="number"
+          value={height}
+          onChange={(e) => setHeight(e.target.value)}
+        />
+      </form>
       <div>
-        <h1>Top Counter</h1>
-        <div>{counter.value}</div>
-        <button onClick={counter.increase}>plus</button>
-        <button onClick={counter.decrease}>minus</button>
-        <button onClick={counter.zero}>zero</button>
+        {name}
+        {born}
+        {height}
       </div>
-
-      <div>
-        <h1>Bottom Counter</h1>
-        <div>{counterBottom.value}</div>
-        <button onClick={counterBottom.increase}>plus</button>
-        <button onClick={counterBottom.decrease}>minus</button>
-        <button onClick={counterBottom.zero}>zero</button>
-      </div>
-    </>
+    </div>
   );
 };
 
